768e0e80d1654ced36d47fa0621f7621e0fde5e6
Maurice Yang <mauriceyangmy28@gmail.com>
2024-04-17 16:58:59 +0700

    Implement delete function in Subscriber repository.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -2,8 +2,9 @@ use crate::model::subscriber::Subscriber;
 use dashmap::DashMap;
 use lazy_static::lazy_static;
 
+// Singleton of Database
 lazy_static! {
-    pub static ref SUBSCRIBERS: Dashmap<String, DashMap<String, Subscriber>> = Dashmap::new();
+    static ref SUBSCRIBERS: DashMap<String, DashMap<String, Subscriber>> = DashMap::new();
 }
 
 pub struct SubscriberRepository;
@@ -12,29 +13,32 @@ impl SubscriberRepository {
     pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
         let subscriber_value = subscriber.clone();
         if SUBSCRIBERS.get(product_type).is_none() {
-            SUBSCRIBERS.insert(product_type.to_string(), Dashmap::new());
-        }
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
+        };
+
         SUBSCRIBERS
             .get(product_type)
             .unwrap()
-            .insert(subscriber.url.clone(), subscriber_value);
-        subscriber
+            .insert(subscriber_value.url.clone(), subscriber_value);
+        return subscriber;
     }
+
     pub fn list_all(product_type: &str) -> Vec<Subscriber> {
         if SUBSCRIBERS.get(product_type).is_none() {
-            return vec![];
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         };
 
         return SUBSCRIBERS
             .get(product_type)
             .unwrap()
             .iter()
-            .map(|x| x.value().clone())
+            .map(|f| f.value().clone())
             .collect();
     }
+
     pub fn delete(product_type: &str, url: &str) -> Option<Subscriber> {
         if SUBSCRIBERS.get(product_type).is_none() {
-            SUBSCRIBERS.insert(product_type.to_string(), Dashmap::new());
+            SUBSCRIBERS.insert(String::from(product_type), DashMap::new());
         };
         let result = SUBSCRIBERS.get(product_type).unwrap().remove(url);
         if !result.is_none() {
